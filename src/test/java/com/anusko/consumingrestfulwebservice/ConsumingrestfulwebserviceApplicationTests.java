25d66e71a6ef8a575f9197bc8bf8dc7eb12b2184
Anupam Kushwaha <anupamkushwaha1426@gmail.com>
2021-09-30 06:13:12 +0530

    Initial commit

---
+++ src/test/java/com/anusko/consumingrestfulwebservice/ConsumingrestfulwebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anusko.consumingrestfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingrestfulwebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
